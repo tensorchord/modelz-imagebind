1bf29f3d7122047bb52ba53a7c87c186917afb9c
cutecutecat <starkind1997@gmail.com>
2023-06-02 15:08:14 +0800

    feat: support llmspec format (#2)
    * feat: support llmspec format
    
    Signed-off-by: cutecutecat <starkind1997@gmail.com>
    
    * fix by suggestions
    
    Signed-off-by: cutecutecat <starkind1997@gmail.com>
    
    * fix request single word
    
    Signed-off-by: cutecutecat <starkind1997@gmail.com>
    
    ---------
    
    Signed-off-by: cutecutecat <starkind1997@gmail.com>

--- example/client.py
+++ example/client.py
@@ -1,6 +1,5 @@
 from typing import List, Literal, TypedDict
-import httpx
-import msgpack
+import modelz
 
 with open("dog_audio.wav", "rb") as f:
     dog_audio_bytes = f.read()
@@ -11,31 +10,56 @@ with open("dog_image.jpg", "rb") as f:
 with open("dog_video.mp4", "rb") as f:
     dog_video_bytes = f.read()
 
-# Change to real endpoint URL and token
-REMOTE_URL = "https://xxx.modelz.tech/"
+# Change to real deployment key and token
+DEPLOYMENT_KEY = "imagebind-zzzzzzzzzzzzzzzz"
 TOKEN = "mzi-ttt"
 
+
 class Input(TypedDict):
-    task: Literal["image", "audio", "video", "text"]
-    data: List[bytes | str]
+    input: List[bytes | str]
+    model: Literal[
+        "imagebind-image",
+        "imagebind-audio",
+        "imagebind-video",
+        "imagebind-text",
+    ]
+
+
+class Embedding(TypedDict):
+    embedding: List[float]
+    index: int
+    object: Literal["embedding"]
+
+
+class Output(TypedDict):
+    data: List[Embedding]
+    model: Literal[
+        "imagebind-image",
+        "imagebind-audio",
+        "imagebind-video",
+        "imagebind-text",
+    ]
+    object: Literal["list"]
+
 
 cases: List[Input] = [
-    {"task":"text", "data": ["A dog", "doggery", "puppy"]},
-    {"task":"image", "data":[dog_image_bytes]},
-    {"task":"audio", "data":[dog_audio_bytes]},
-    {"task":"video", "data":[dog_video_bytes]},
+    {"model": "imagebind-text", "input": ["A dog", "doggery", "puppy"]},
+    {"model": "imagebind-image", "input": [dog_image_bytes]},
+    {"model": "imagebind-audio", "input": [dog_audio_bytes]},
+    {"model": "imagebind-video", "input": [dog_video_bytes]},
 ]
 
+# To test locally, add the `host` argument
+# client = modelz.ModelzClient(
+#     key=TOKEN, deployment=DEPLOYMENT_KEY, timeout=60, host="http://127.0.0.1:8080"
+# )
+client = modelz.ModelzClient(key=TOKEN, deployment=DEPLOYMENT_KEY, timeout=60)
+
 for case in cases:
-    prediction = httpx.post(
-        f"{REMOTE_URL}/inference",
-        headers={"X-API-Key": TOKEN},
-        data=msgpack.packb(case),
-        timeout=httpx.Timeout(timeout=60.0)
-    )
-    if prediction.status_code == 200:
-        data = msgpack.unpackb(prediction.content)
-        print(f'{str(data)[:50]}...{str(data)[-50:]}')
-    else:
-        print(prediction.status_code)
-        print(prediction.content)
+    print("case for task {}:".format(case["model"]))
+    prediction: Output = client.inference(case, serde="msgpack").data
+    embeddings = prediction["data"]
+    for emb in embeddings:
+        print(
+            f"{emb['index']}-{str(emb['embedding'])[:50]}...{str(emb['embedding'])[-50:]}"
+        )
